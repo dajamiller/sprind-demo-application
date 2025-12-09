53b9d4721a9fa69bd361c61e9a267e1572a2c973
Daja Miller <dajamiller96@gmail.com>
2025-12-09 15:15:28 -0800

    Add files via upload

---
+++ musiccatalog.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE musiccatalog;
+-- USE musiccatalog;
+/*CREATE TABLE artists (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    album_title VARCHAR(200) NOT NULL,
+    song_title VARCHAR(200) NOT NULL
+);*/
+/*INSERT INTO artists (name, album_title, song_title) VALUES
+('The Weeknd', 'After Hours', 'Blinding Lights'),
+('Adele', '21', 'Rolling in the Deep'),
+('Coldplay', 'Parachutes', 'Yellow');*/
+
